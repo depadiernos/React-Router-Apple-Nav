a78acc1eb7db6801f01bfdf086fc3a778d84fc58
Dennis Padiernos <depadiernos@gmail.com>
2019-09-07 14:10:02 -0500

    make the menus look a tiny bit better.

--- apple-nav/src/components/MainNav.js
+++ apple-nav/src/components/MainNav.js
@@ -1,24 +1,55 @@
-import React from 'react'
-import { Route, NavLink } from 'react-router-dom'
-import SubNav from './SubNav'
-import Home from './Home'
-import About from './About'
-import { nav } from '../utils/menu-data'
+import React from "react"
+import { Route, NavLink } from "react-router-dom"
+import styled from "styled-components"
+import SubNav from "./SubNav"
+import Home from "./Home"
+import About from "./About"
+import { nav } from "../utils/menu-data"
 
-export default function () {
+const MainNav = styled.nav`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  a {
+    padding: 1rem;
+  }
+`
+
+export default function() {
   return (
     <div>
-      <nav>
+      <MainNav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/cars">Cars</NavLink>
         <NavLink to="/motorcycles">Motorcycles</NavLink>
         <NavLink to="/trucks">Trucks</NavLink>
         <NavLink to="/about">About</NavLink>
-      </nav>
+      </MainNav>
       <Route path="/" exact component={Home} />
-      <Route path="/cars" render={props => <SubNav {...props} pathname={"cars"} navItems={nav.cars} />} />
-      <Route path="/motorcycles" render={props => <SubNav {...props} pathname={"motorcycles"} navItems={nav.motorcycles} />} />
-      <Route path="/trucks" render={props => <SubNav {...props} pathname={"trucks"} navItems={nav.trucks} />} />
+      <nav>
+        <Route
+          path="/cars"
+          render={(props) => (
+            <SubNav {...props} pathname={"cars"} navItems={nav.cars} />
+          )}
+        />
+        <Route
+          path="/motorcycles"
+          render={(props) => (
+            <SubNav
+              {...props}
+              pathname={"motorcycles"}
+              navItems={nav.motorcycles}
+            />
+          )}
+        />
+        <Route
+          path="/trucks"
+          render={(props) => (
+            <SubNav {...props} pathname={"trucks"} navItems={nav.trucks} />
+          )}
+        />
+      </nav>
       <Route path="/about" component={About} />
     </div>
   )
--- apple-nav/src/components/SubNav.js
+++ apple-nav/src/components/SubNav.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import { Route, NavLink } from 'react-router-dom'
+import styled from 'styled-components'
 import NavItem from './NavItem'
 
+
+const SubNav = styled.nav`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  a {
+    padding: 1rem;
+  }
+`
 
 export default function (props) {
   console.log(props)
   return (
     <div>
-      <nav>
+      <SubNav>
         {(props) && props.navItems.map((navItem) => {
           return (
             <NavLink key={navItem.id} to={`/${props.pathname}/${navItem.id}`}>{navItem.model}</NavLink>
           )
         })}
-      </nav>
+      </SubNav>
       {(props) && props.navItems.map((navItem) => {
         return (
           <Route key={navItem.id} path={`/${props.pathname}/${navItem.id}`} render={(props) => <NavItem {...props} details={navItem} />} />
