9c0df0372f84badce9d0cfb75728eb84882935e7
Dennis Padiernos <depadiernos@gmail.com>
2019-09-07 15:00:59 -0500

    add item image

--- apple-nav/src/components/NavItem.js
+++ apple-nav/src/components/NavItem.js
@@ -2,5 +2,8 @@ import React from 'react'
 
 export default function(props){
   console.log(props)
-  return (<div>{`${props.details.make}, ${props.details.model}`}</div>)
+  return (<div>
+    <h1>{`${props.details.make}, ${props.details.model}`}</h1>
+    <img src={props.details.image} alt={props.details.model}/>
+    </div>)
 }
